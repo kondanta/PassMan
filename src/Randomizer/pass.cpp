6831a5704d091ccdd20b0752ae1e6a7c6f35f21b
Taylan Dogan <16501501+kondanta@users.noreply.github.com>
2018-01-03 19:34:56 +0300

    New Folder Structure

---
+++ src/Randomizer/pass.cpp
@@ -0,0 +1,63 @@
+#include "pass.hpp"
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+
+void PassGen::SetDigit(int flag) { digit = flag; }
+
+void PassGen::SetSymbol(int flag) { symbols = flag; }
+
+int PassGen::GetDigit() { return digit; }
+
+int PassGen::GetSymbol() { return symbols; }
+
+void PassGen::setPassword(std::string pass) { generatedPassword = pass; }
+
+std::string PassGen::getPassword() { return generatedPassword; }
+
+void PassGen::HelpMenu(std::string argument) {
+  std::cerr << "Usage for " << argument << '\n'
+            << "-d, --digit\t for numbers [0..9]\n"
+            << "-s, --symbols\t for !@#$%.."
+            << "--size [no]\t Generating passwords by given size.";
+}
+
+// FIXME: CCN is 7, try to fix it.
+std::vector<char> CreateContainer(int dflag, int sflag) {
+  // Flag option digit
+  std::vector<char> digitals = {'0', '1', '2', '3', '4',
+                                '5', '6', '7', '8', '9'};
+  // base vector to be used for password generation
+  std::vector<char> letters = {
+      'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+      'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+      'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+      'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+  // Flag option symbols.
+  std::vector<char> metaChars = {
+      '!', '#', '$', '%', '^', '&', '*', '(',  ')', '+', '-', '.', '/', ':',
+      ';', '<', '=', '>', '?', '@', '[', '\\', ']', '_', '{', '|', '}', '~'};
+  if (dflag == 1 && sflag == 1) {
+    letters.insert(letters.begin(), digitals.begin(), digitals.end());
+    letters.insert(letters.begin(), metaChars.begin(), metaChars.end());
+  } else if (dflag == 1 && sflag == 0) {
+    letters.insert(letters.begin(), digitals.begin(), digitals.end());
+  } else if (dflag == 0 && sflag == 1) {
+    letters.insert(letters.begin(), metaChars.begin(), metaChars.end());
+  }
+  return letters;
+}
+
+void PassGen::GeneratePassword(int dflag, int sflag, int size = 15) {
+  // Generating the container for the password generation.
+  std::vector<char> passContainer = CreateContainer(dflag, sflag);
+  std::string pass;
+  // Randomizing the generation each time it called!
+  srand(time(NULL));
+
+  for (int i = 0; i < size; i++) {
+    int x = rand() % passContainer.size();
+    pass += passContainer.at(x);
+  }
+  setPassword(pass);
+}
