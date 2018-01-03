261f6e8f8873d18c110c8f7b952b1fc6eb46cc05
Taylan Dogan <16501501+kondanta@users.noreply.github.com>
2018-01-03 19:33:12 +0300

    Generate Pass Test

--- tests/pass-test.cpp
+++ tests/pass-test.cpp
@@ -1,11 +1,18 @@
-#include "../inc/pass.hpp"
-#include "../src/pass.cpp"
+#include "../src/Randomizer/pass.hpp"
 #include "gtest/gtest.h"
 
-TEST(Testing, Testing) {
+TEST(SetandGet, SetAndGet) {
   PassGen obj;
   obj.setPassword("asd");
   EXPECT_EQ("asd", obj.getPassword());
+}
+
+TEST(GeneratePass, PassGen) {
+  PassGen obj;
+  std::string a;
+  obj.GeneratePassword(1, 1, 5);
+  a = obj.getPassword();
+  EXPECT_EQ(5, a.length());
 }
 
 int main(int argc, char *argv[]) {
