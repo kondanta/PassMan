1eb5c351ffe7b5e3a2b7390061fdd5545360d809
Taylan Dogan <16501501+kondanta@users.noreply.github.com>
2018-01-03 17:54:10 +0300

    Rewrite Testing

--- tests/pass-test.cpp
+++ tests/pass-test.cpp
@@ -1,26 +1,14 @@
-#define BOOST_TEST_MODULE addertest
-#include <boost/test/included/unit_test.hpp>
-#include "pass.hpp"
+#include "../inc/pass.hpp"
+#include "../src/pass.cpp"
+#include "gtest/gtest.h"
 
-using namespace std;
-
-struct TestFixture{
-    PassGen obj;
-    TestFixture()
-    : obj()
-    {BOOST_TEST_MESSAGE("setup fixture");};
-};
-
-
-BOOST_FIXTURE_TEST_SUITE(TestGet, TestFixture);
-
- BOOST_AUTO_TEST_CASE(test1)
-{   
-    BOOST_TEST_MESSAGE("Checking the password methods");
-    PassGen obj;
-    string pass = "t";
-    obj.setPassword(pass);
-    BOOST_CHECK_EQUAL (obj.getPassword(), "t");
+TEST(Testing, Testing) {
+  PassGen obj;
+  obj.setPassword("asd");
+  EXPECT_EQ("asd", obj.getPassword());
 }
 
-BOOST_AUTO_TEST_SUITE_END();
+int main(int argc, char *argv[]) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
